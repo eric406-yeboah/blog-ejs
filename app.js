5199cad6105c336f18e509cee60d5763c6979848
eric406-yeboah <ericusyeboah406@gmail.com>
2024-01-31 10:33:09 +0000

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express= require('express')
 
 const app = express()
 
-const port = 5000
+const port = process.env.Port||5000
 
 app.set('view engine','ejs')
 app.use(express.static(__dirname + '/public'));
